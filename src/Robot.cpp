dfd38be728cefdafc2951f7aa1e45a270ad6a1b2
Ross Bajocich <r.bajocich@gmail.com>
2016-01-20 11:24:14 -0800

    Added new 2016 Gyro
    They have updated the driver and headers for 2016, so new installation
    is required. Follow the instructions in HowToInstallNavX.txt in order to
    have a successful compilation.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,14 +1,21 @@
-#include "WPILib.h"
-#include "Commands/Command.h"
-#include "CommandBase.h"
+#include <CommandBase.h>
+#include <Commands/Scheduler.h>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <RobotBase.h>
+#include <SerialPort.h>
+
+#include "../navx-mxp/cpp/include/AHRS.h"
 
 class Robot: public IterativeRobot {
 private:
 	LiveWindow *lw;
+	AHRS *gyro;
 
 	void RobotInit() {
 		CommandBase::init();
 		lw = LiveWindow::GetInstance();
+		gyro = new AHRS(SerialPort::kMXP); /* Alternatives:  SPI::kMXP, I2C::kMXP or SerialPort::kUSB */
 	}
 
 	void DisabledPeriodic() {
