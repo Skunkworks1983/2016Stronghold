52146424e4a86d0ac168c299fd8aaa89275add3c
Eli Benevedes <benevedeselias@gmail.com>
2016-01-20 20:58:57 -0800

    Created MotorManager to manage motors, not actually managed yet just another layer right now

--- src/Subsystems/Drivebase.cpp
+++ src/Subsystems/Drivebase.cpp
@@ -5,21 +5,10 @@
 
 Drivebase::Drivebase() :
 		Subsystem("Drivebase") {
-	leftMotor1 = new Talon(DRIVEBASE_LEFTMOTOR_1_PORT);
-	leftMotor2 = new Talon(DRIVEBASE_LEFTMOTOR_2_PORT);
-	leftMotor3 = new Talon(DRIVEBASE_LEFTMOTOR_3_PORT);
-	rightMotor1 = new Talon(DRIVEBASE_RIGHTMOTOR_1_PORT);
-	rightMotor2 = new Talon(DRIVEBASE_RIGHTMOTOR_2_PORT);
-	rightMotor3 = new Talon(DRIVEBASE_RIGHTMOTOR_3_PORT);
+		motorManager = MotorManager::getMotorManager();
 }
 
 Drivebase::~Drivebase() {
-	delete leftMotor1;
-	delete leftMotor2;
-	delete leftMotor3;
-	delete rightMotor1;
-	delete rightMotor2;
-	delete rightMotor3;
 }
 
 void Drivebase::InitDefaultCommand() {
@@ -28,17 +17,7 @@ void Drivebase::InitDefaultCommand() {
 	SetDefaultCommand(new TankDrive());
 }
 
-void Drivebase::setLeftSpeed(double speed) {
-	leftMotor1->Set(-speed);
-	leftMotor2->Set(-speed);
-	leftMotor3->Set(-speed);
-}
 
-void Drivebase::setRightSpeed(double speed) {
-	rightMotor1->Set(speed);
-	rightMotor2->Set(speed);
-	rightMotor3->Set(speed);
-}
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
 
@@ -47,10 +26,18 @@ void Drivebase::resetEncoder()
 
 }
 
+void Drivebase::setLeftSpeed(double speed) {
+	motorManager->setLeftSpeed(speed);
+}
+
+void Drivebase::setRightSpeed(double speed) {
+	motorManager->setRightSpeed(speed);
+}
+
 float Drivebase::getRightDistance(){
 	return 0; //TODO change this
 }
 
 float Drivebase::getLeftDistance(){
 	return 0;
-}
+} //Maybe move to somewhere else. Not now -Eli
