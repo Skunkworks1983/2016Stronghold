7d69a3e9e6adad60e760529fcdd05e4f37ae6a00
Ailis Waddill <aviatorailis@gmail.com>
2016-02-08 18:31:31 -0800

    Logger and OI changes. Probably doesn't break anything -BG

---
+++ src/Services/Logger.cpp
@@ -0,0 +1,32 @@
+#include "Logger.h"
+#include <ctime>
+#include <fstream>
+#include <string>
+
+void writeToLogFile (std::string fileName, std::string message)
+{
+	std::ofstream logFile;
+	logFile.exceptions(std::ifstream::failbit | std::ifstream::badbit );
+	try
+	{
+		logFile.open(fileName.c_str(), std::ios::app);
+		time_t timer = time(NULL);
+		logFile<<timer<<" "<<message<<std::endl;
+	}
+	catch(std::ifstream::failure e)
+	{
+
+	}
+}
+//add thing
+
+//int main (int argc, char** argv)
+//{
+	//writeToLogFile("/U/fileNameDos", "robotDos");
+	//return 0;
+//}
+
+
+
+
+
