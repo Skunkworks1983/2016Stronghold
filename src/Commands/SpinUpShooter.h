3ac07c904767312b77b7d2fdd035934bc14b6d9a
Tbowlin <tuckerdrew@hotmail.com>
2016-02-09 19:37:39 -0800

    added from the stash- spin up shooter

---
+++ src/Commands/SpinUpShooter.h
@@ -0,0 +1,36 @@
+#ifndef SpinUpShooter_H
+#define SpinUpShooter_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+#include "Subsystems/MotorManager.h"
+#include "OI.h"
+#include "Subsystems/SensorManager.h"
+
+class SpinUpShooter: public CommandBase, PIDSource, PIDOutput
+{
+
+private:
+	OI            * oi;
+	MotorManager  * motormanager;
+	SensorManager * sensormanager;
+	PIDController * pid;
+
+
+public:
+	SpinUpShooter();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	float P;
+	float I;
+	float D;
+	float speed;
+	double PIDGet();
+	void PIDWrite(float output);
+
+};
+
+#endif
