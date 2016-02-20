7f7dcede849355f16cf337cf7ecb01d27253a85f
linknmike <s-4032218@AVI109878SN.ERAC.HSD.PRI>
2016-02-20 10:17:45 -0800

    Finished doMoat and doRoughT
    Also put sensorManager in AutoBase

--- src/Commands/Autonomous/doMoat.cpp
+++ src/Commands/Autonomous/doMoat.cpp
@@ -2,8 +2,13 @@
 
 AutoBase *AutoBase::doMoat()
 {
+	sensorManager = SensorManager::getSensorManager();
+
 	AutoBase *cmd = new AutoBase("Autonomous-doMoat");
 	//cmd->AddSequential(new DriveForward(myCommandParameters));
 		//Add commands here
+	initialPosition = sensorManager->getYaw();
+	AddSequential(new DriveForward(5, 1));
+	AddSequential(new TurnDegree(sensorManager->getYaw() - initialPosition));
 	return cmd;
 }
--- src/Commands/Autonomous/doRoughT.cpp
+++ src/Commands/Autonomous/doRoughT.cpp
@@ -5,6 +5,11 @@ AutoBase *AutoBase::doRoughT()
 	AutoBase *cmd = new AutoBase("Autonomous-doRoughT");
 	//cmd->AddSequential(new myCommand(myCommandParameters));
 		//Add commands here
+	sensorManager = SensorManager::getSensorManager();
+	initialPosition = sensorManager->getYaw();
+	AddSequential(new DriveForward(5, 1));
+	AddSequential(new TurnDegree(sensorManager->getYaw() - initialPosition));
+	return cmd;
 	return cmd;
 }
 
