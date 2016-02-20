13cf34094c1ed2e8c2b3cab882803ed2c5d8f991
linknmike <s-4032218@AVI109878SN.ERAC.HSD.PRI>
2016-02-20 11:54:03 -0800

    Fixed merge conflicts in SpinUpShooter

--- src/Commands/Shooting/SpinUpShooter.cpp
+++ src/Commands/Shooting/SpinUpShooter.cpp
@@ -2,34 +2,24 @@
 #include <OI.h>
 #include <Services/MotorManager.h>
 #include <Services/SensorManager.h>
-<<<<<<< HEAD:src/Commands/Shooting/SpinUpShooter.cpp
 
-SpinUpShooter::SpinUpShooter()
-=======
-//
 SpinUpShooter::SpinUpShooter(float timeOut, float speed)
->>>>>>> refs/remotes/origin/collector:src/Commands/SpinUpShooter.cpp
 {
 
 	oi = new OI();
 	motorManager = MotorManager::getMotorManager();
 	sensorManager = SensorManager::getSensorManager();
-<<<<<<< HEAD:src/Commands/Shooting/SpinUpShooter.cpp
-=======
+
 	this->timeOut = timeOut;
 
 
 
->>>>>>> refs/remotes/origin/collector:src/Commands/SpinUpShooter.cpp
 }
 
 // Called just before this Command runs the first time
 void SpinUpShooter::Initialize()
 {
-<<<<<<< HEAD:src/Commands/Shooting/SpinUpShooter.cpp
 
-=======
->>>>>>> refs/remotes/origin/collector:src/Commands/SpinUpShooter.cpp
 
 SetTimeout(timeOut);
 }
